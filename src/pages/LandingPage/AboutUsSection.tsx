720db4f4d732174f1e6bbd4d027bde06c5487a04
AngleSad <118389540+AngleSad@users.noreply.github.com>
2024-02-20 21:52:01 +0100

    About us
    Images + Text

--- src/pages/LandingPage/AboutUsSection.tsx
+++ src/pages/LandingPage/AboutUsSection.tsx
@@ -1,20 +1,20 @@
-import Vue from '@/assets/vue.png';
-import React from '@/assets/react.png';
-import Tailwind from '@/assets/tailwind.png';
+import Vue from '@/assets/npm.png';
+import React from '@/assets/web.png';
+import Tailwind from '@/assets/templates.png';
 import { AboutUsProps } from './interfaces';
 
 const AboutUsContent = [
   {
     img: Vue,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste amet blanditiis quos! Rem sapiente voluptates iusto? Laboriosam, eos perspiciatis temporibus rerum eveniet consectetur amet rem doloribus, eum quis ex sapiente!',
+    text: "With this amazing npm package, you'll be able to unleash your creativity and effortlessly customize these templates with just one command! It's an exhilarating opportunity to elevate your projects to new heights!",
   },
   {
     img: React,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste amet blanditiis quos! Rem sapiente voluptates iusto? Laboriosam, eos perspiciatis temporibus rerum eveniet consectetur amet rem doloribus, eum quis ex sapiente!',
+    text: "In this amazing web platform, you'll be able to manage your published websites and upload the templates you love the most! Imagine the endless possibilities to customize and bring your online projects to life!",
   },
   {
     img: Tailwind,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste amet blanditiis quos! Rem sapiente voluptates iusto? Laboriosam, eos perspiciatis temporibus rerum eveniet consectetur amet rem doloribus, eum quis ex sapiente!',
+    text: "Embark on a journey through endless possibilities! Explore an unimaginable number of templates, crafted by a passionate community just like you. Find the perfect template that resonates with your vision and unleash your full creative potential!",
   },
 ];
 
@@ -25,7 +25,7 @@ const AboutUsSection: React.FC<AboutUsProps> = ({ darkMode }) => {
         About Us
       </h2>
 
-      <article className="flex flex-col items-center gap-10 md:flex-col md:items-start">
+      <article className="flex flex-col items-center gap-2  md:flex-col md:items-start">
         {AboutUsContent.map((content, index) => (
           <div
             key={index}
