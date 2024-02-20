512c3fa539c76d16eb7553c2884c5ff4ee061e1f
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-02-20 12:55:20 +0100

    Add DynamicIsland component and update login UI

--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -1,4 +1,5 @@
 import github from '@icons/github.svg';
+import DynamicIsland from "@/components/ui/dynamicIsland";
 import { useState } from 'react';
 
 function AuthPage() {
@@ -24,21 +25,28 @@ function AuthPage() {
     <main>
         <div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
+        <DynamicIsland />
+
         <div className='flex h-screen'>
             {/* Left Block */}
-            <div className='hidden md:flex w-2/3 md:w-0 flex-col justify-center items-center'>
+            <div className='hidden md:flex w-0 md:w-2/3 flex-col justify-center items-center'>
                 <h1 className="text-4xl font-bold text-center pt-20 text-white">Welcome to OpenLayout</h1>
             </div>
             
             {/* Login Block */}
-            <div className='w-2/3 md:w-full backdrop-brightness-50 backdrop-blur-3xl border border-gray-700 rounded-lg m-2'>
-                <h1 className='flex text-white'>Login</h1>
+            <div className={(authUrl && 'group ') + ' flex justify-center items-center w-full md:w-1/3 backdrop-brightness-50 backdrop-blur-3xl border border-gray-700 rounded-lg m-2'}>
+              <div className="flex flex-col items-center text-center">
+                <h1 className='text-white text-7xl font-bold mb-8'>Login</h1>
+                <p className='text-gray-500 mb-16'>Click the button below to log in with your GitHub account.</p>
                 
-                  <button className={`flex px-6 py-4 text-white rounded-xl border-2 transition duration-1000 ${authUrl ? 'group bg-green-500 border-green-400' : 'bg-gray-600 border-gray-500 cursor-not-allowed'} items-center font-bold group-hover:border-4`}>
-                      <img src={github} alt="GitHub Logo" className='flex w-8 mr-2 group-hover:translate-x-52 rotate-full group-hover:rotate-full transition duration-300' />
-                      <span className='group-hover:-translate-x-10 transition duration-300'>Authenticate With GitHub</span>
-                  </button>
-            </div>
+                <button className={`flex px-6 mx-2 py-4 text-white rounded-xl border-2 transition-all duration-1000 ${authUrl ? 'bg-green-500 border-green-400' : 'bg-gray-600 border-gray-500 cursor-not-allowed'} items-center font-bold hover:scale-105`}>
+                  <img src={github} alt="GitHub Logo" className='flex w-8 mr-2 group-hover:translate-x-44 rotate-full group-hover:rotate-full transition duration-300' />
+                  <span className='group-hover:-translate-x-3 transition duration-300'>Authenticate With <span className='group-hover:invisible'>GitHub</span></span>
+                </button>
+
+                <span className='mt-2 text-gray-500 text-sm'>I don want to log-in, <a href="#" className='text-indigo-600 font-bold'>go back</a></span>
+              </div>
+          </div>
         </div>
     </main>
   );
