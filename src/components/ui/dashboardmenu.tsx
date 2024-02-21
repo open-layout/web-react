bbc091844e4bfee6201b2c60c10ea986ff90b013
Gonzalo Azaldegi <azldgi03@gmail.com>
2024-02-21 12:36:41 +0100

    Moved switch to Dashboard Menu

--- src/components/ui/dashboardmenu.tsx
+++ src/components/ui/dashboardmenu.tsx
@@ -1,19 +1,20 @@
-
+import Switch from '@/components/ui/Switch';
 
 const DashboardMenu = () => {
   return (
     <nav
-      className="menu-container grid place-items-center "
-      style={{ zIndex: 999 }}>
-      <div className="fixed left-10 top-60 bg-gray-700/40 border-gray-700/50 border rounded-md  py-1 px-2 mt-3">
+      className="fixed left-10 top-60 menu-container grid place-items-center">
+
+      <div className="flex flex-col items-center bg-gray-700/40 border-gray-700/50 border rounded-md  py-1 px-2 mt-3">
+      <Switch />
         <div className="flex flex-col items-center select-none text-white gap-5 p-4">
-        
-          <div className='flex flex-row'>
+          <div className="flex flex-row">
             <a href="#" className="ml-1 pr-1">
-            Search: 
-          </a>
-          <input className='rounded-full w-36' /></div>
-          
+              Search:
+            </a>
+            <input className="rounded-full w-36" />
+          </div>
+
           <a href="#" className="ml-1 pr-1">
             Filter
           </a>
