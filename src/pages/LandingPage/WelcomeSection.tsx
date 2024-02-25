c40a9a2996994c51ec0a13edf03b84de4ee35286
Gonzalo Azaldegi <98758892+AzalDevX@users.noreply.github.com>
2024-02-25 20:19:41 +0100

    Fixed sm laptop view

--- src/pages/LandingPage/WelcomeSection.tsx
+++ src/pages/LandingPage/WelcomeSection.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const WelcomeSection: React.FC = () => {
   return (
-    <section className="grid place-items-center lg:mt-60 mt-32">
+    <section className="grid place-items-center xl:mt-60 mt-32">
       <div>
         <h1 className="text-4xl mb-8 text-center justify-center items-center font-poppins sm:text-6xl lg:flex-row flex flex-col">
           Welcome to&nbsp;
           <span className="text-title font-bold">Open Layouts</span>
         </h1>
       </div>
-      <p className="text-gray-400 font-raleway pl-5 w-[80%] lg:text-left text-balance text-md lg:w-[50%] sm:pl-14 sm:text-xl">
+      <p className="text-gray-400 font-raleway pl-5 w-[80%] xl:text-left text-center text-md xl:w-[50%] sm:pl-14 sm:text-xl">
         An incredible platform for discovering the perfect{' '}
         <strong className="text-title/80">open-source</strong> layout for your
         personal website, offering a wide array of professional and high-quality
