a848041077fdbccf3893d8f70e8febc1e25d94f4
Gonzalo Azaldegi <azldgi03@gmail.com>
2024-02-27 12:54:35 +0100

    Updated developers cards

--- src/components/ui/presentationCard.tsx
+++ src/components/ui/presentationCard.tsx
@@ -26,27 +26,24 @@ const PresentationCard: React.FC<PresentationCardProps> = ({
   }, [badges]);
 
   return (
-    <article className="group text-white backdrop-blur-md border border-title px-2 py-4 rounded-xl w-72 h-72 relative flex flex-col justify-end gap-4 hover:shadow-lg hover:shadow-title transition-shadow duration-300">
+    <div className="select-none group flex flex-col justify-start items-start gap-2 w-96 h-56 duration-500 relative rounded-lg p-4 bg-black/60 border-2 border-gray-900  backdrop-blur-xl hover:-translate-y-2 hover:shadow-xl shadow-gray-800">
       <img
-        className="w-20 rounded-[50px] absolute -top-3 -left-3 border border-title shadow-2xl group-hover:rounded-3xl transition-all group-hover:shadow-lg group-hover:shadow-title"
         src={image}
         alt={`${name}'s pfp`}
+        className="absolute duration-700 shadow-md group-hover:-translate-y-4 group-hover:-translate-x-4 -bottom-5 -right-5 w-20 rounded-lg bg-gray-800"
       />
-      <div className="self-end flex flex-col">
-        <div className='flex flex-row self-end bg-gray-700 p-1 mt-1 rounded-sm'>
-          {badgeUrls.map((url, index) => (
-            <img key={badges[index].id} src={url} alt={badges[index].id} className='w-5 mb-1'/>
-          ))}
-        </div>
-        <p className="text-xs text-gray-600">Since {date.split('-')[0]}</p>
+
+      <div className="">
+        <h2 className="text-2xl font-bold mb-2 text-gray-100">{name}</h2>
+        <p className="text-gray-200 line-clamp-3">{description}</p>
       </div>
-      <div className="p-2 flex flex-col bg-title/80 rounded-md min-h-48">
-        <h2 className="text-xl pl-3">{name}</h2>
-        <hr className="m-1" />
-        <p className="text-gray-400 text-sm pl-3">{description}</p>
-      </div>
-      <a href={link} target="_blank" className=""></a>
-    </article>
+      <a
+        href={link}
+        target="_blank"
+        className="hover:bg-gray-800 bg-gray-900 text-gray-100 transition-colors duration-200 mt-6 rounded p-2 px-6 absolute bottom-5 left-5">
+        Github
+      </a>
+    </div>
   );
 };
 
