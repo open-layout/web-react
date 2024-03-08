69d7234688aaa21e55bbc3919eea8c1952c3e8ef
jonarango <arangojond@gmail.com>
2024-03-08 12:49:31 +0100

    Add target attribute to Get Started button. To test branch protection and merging

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -84,6 +84,7 @@ function LandingPage() {
       <div className="lg:pt-32 pt-20 flex flex-row gap-5 justify-center">
         <a
           className="px-8 py-3 dark:bg-white dark:text-black bg-black text-white rounded-full shadow-inner-xl font-semibold"
+          target='_blank'
           href="https://docs.openlayout.me">
           Get Started
         </a>
